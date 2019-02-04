244936d7cc389106aff0f98106a5e0d4777f8b8a
Ishwariya18 <47315298+Ishwariya18@users.noreply.github.com>
2019-02-04 00:22:56 -0800

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter the value of n");
+scanf("%d",&n);
+if(n>0)
+{
+printf("n is positive");
+}
+elif(n<0)
+{
+printf("n is negative");
+}
+else
+{
+printf("n is zero");
+}
+}
