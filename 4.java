f03681e140147349113f8ad11c9fef927ff6ce42
Ishwariya18 <47315298+Ishwariya18@users.noreply.github.com>
2019-03-04 21:43:51 -0800

    Create 4.java

---
+++ 4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class alphabet
+{
+public static void main(String args[])
+{
+char ch;
+System.out.println("enter he value of ch:");
+Scanner in=new Scanner(System.in)
+ch=in.nextInt();
+charAt(0);
+if(ch>='a'&& ch<='z')||(ch>='A'&& ch<='Z')
+{
+System.out.println("Alphabet");
+}
+else
+{
+System.out.println("No");
+}
+}
+}
+
+
