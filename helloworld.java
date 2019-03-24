98b5f64220cc8ca04dc16d4bf990c4b274793564
Ishwariya18 <47315298+Ishwariya18@users.noreply.github.com>
2019-03-23 23:06:01 -0700

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -5,7 +5,7 @@ BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 String line=br.readLine();
 int N=Integer.parseInt(line);
 for(int i=0;i<N;i++){
-System.out.println("hello world");
+System.out.println("Hello");
 }
 }
 }
