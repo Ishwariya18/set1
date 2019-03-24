2637af045a06a3e8e71e9ef68aeaa7543628e09d
Ishwariya18 <47315298+Ishwariya18@users.noreply.github.com>
2019-03-23 23:34:14 -0700

    Create digits.java

---
+++ digits.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.Scanner;
+class digits
+{
+public static void main(String args[])
+{
+int num,sum=0;
+Scanner in=new Scanner(System.in);
+num=in.nextLine();
+
+for(int i=1;i<=num;++i)
+{
+sum+=i;
+}
+System.out.println("sum="+sum);
+}
+}
