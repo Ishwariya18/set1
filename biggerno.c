819b026c873bb600aa5a6d156c5695e8182fc08d
Ishwariya18 <47315298+Ishwariya18@users.noreply.github.com>
2019-03-25 06:00:24 -0700

    Create biggerno.c

---
+++ biggerno.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char str[10],str1[10];
+int i,j,len,count=0;
+scanf("%s",str);
+len=strlen(str);
+for(i=len-1;j=0;i>=0;i--;j++)
+{
+str1[j]=str[i];
+count++;
+}
+prinftf("%d",count-(len-1));
+}
