bbdaebfd4178d6f30ec64eee5256efba74c9a5e4
Ishwariya18 <47315298+Ishwariya18@users.noreply.github.com>
2019-03-25 05:20:41 -0700

    Create partition.c

---
+++ partition.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdbool.h>
+bool is SubsetSum(int arr[],int n,int sum)
+{
+if(sum==0)
+{
+return true;
+}
+if(n==0 && sum!=0)
+{
+return false;
+}
+if(arr[n-1]>sum)
+{
+return isSubsetSum(arr,n-1,sum)||isSubsetSum(arr,n-1,sum-arr[n-1]);
+}
+}
+bool findPartition(int arr[],int n)
+{
+int sum=0;
+for(int i=0;i<n;i++)
+sum+=arr[i];
+if(sum%2!=0)
+{
+return false;
+}
+return isSubsetSum(arr,n,sum/2)
+}
