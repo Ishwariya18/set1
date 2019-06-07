8291ccd8ebd2e7d19ad102d8beb0b031447ad084
Ishwariya18 <47315298+Ishwariya18@users.noreply.github.com>
2019-06-07 05:44:12 -0700

    Update pro2 3.py

--- pro2 3.py
+++ pro2 3.py
@@ -1,6 +1,6 @@
-num,n=map(int,input().split())
-lis=list(map(int,input(0.split()))
-for i in range(n):
-    u,v=map(int,input().split())
-    c=lis[u-1:v]
-    print(min(c))
+    num,k=map(int,input().split())
+    lis=list(map(int,input().split()))
+    for i in range(k):
+        u,v=map(int,input().split())
+        c=lis[u-1:v]
+        print(min(c))
