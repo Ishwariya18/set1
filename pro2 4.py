b4444acb8c5e66d7f37a3e57ae6871795829fd9e
Ishwariya18 <47315298+Ishwariya18@users.noreply.github.com>
2019-06-09 19:34:07 -0700

    Update pro2 4.py

--- pro2 4.py
+++ pro2 4.py
@@ -6,7 +6,7 @@ l7=[]
 for i in range(k):
   x=[int(x) for x in input().split()]
   l7.append(x)
-for i in range(len(17)):
+for i in range(len(l7)):
   k=l7[i][0]
   j=l7[i][1]
   x=reduce(xor,l[k-1:j])
