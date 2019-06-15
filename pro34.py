9e339f5d3fcc250e2de590825fb82f1a79c78318
Ishwariya18 <47315298+Ishwariya18@users.noreply.github.com>
2019-06-14 23:59:41 -0700

    Update pro34.py

--- pro34.py
+++ pro34.py
@@ -1,10 +1,10 @@
 nu=int(input())
 a=[]
-k=bin(2**nu-1)[2::1]
+k=bin(2**nu-1)[2::]
 l=len(k)
 for i in range(0,2**nu):
     s=bin(i)[2::]
-    if len(s)<1:
+    if len(s)<l:
         a.append([s.count("1"),(l-len(s))*"0"+s])
     else:
         a.append([s.count("1"),s])
