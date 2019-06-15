7e996b0c088010c0456e64fb329d0b294c4c4c17
Ishwariya18 <47315298+Ishwariya18@users.noreply.github.com>
2019-06-15 10:26:35 -0700

    Update pro31.py

--- pro31.py
+++ pro31.py
@@ -7,7 +7,7 @@ z=[]
 for i in range(0,d):
   z.append(sorted(list(map(int,input().split()))))
 z=sorted(z)
-for i in rage(0,len(z[0])):
+for i in range(0,len(z[0])):
   for j in range(0,len(z)-1):
     if z[j][i]>z[j+1][i]:
       z[j][i],z[j+1][i]=z[j+1][i],z[j][i]
