293746fbe7f8f10fcd81b9261ecc56611c7b3e02
Ishwariya18 <47315298+Ishwariya18@users.noreply.github.com>
2019-06-16 03:27:14 -0700

    Update pro42.py

--- pro42.py
+++ pro42.py
@@ -1,8 +1,8 @@
-t,s=map(int,input().split())
-l=list(map(int,input().split()))
-if s==1:
-    print(min(l))
-elif s==2:
-    print(max(l[0],l[t-1]))
+u,v=map(int,input().split())
+x=list(map(int,input().split()))
+if v==1:
+    print(min(x))
+elif v==2:
+    print(max(x[0],x[u-1]))
 else:
-    print(max(l))
+    print(max(x))
