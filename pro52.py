44872decdf543093741c4682d1c2f4fdad8e73eb
Ishwariya18 <47315298+Ishwariya18@users.noreply.github.com>
2019-06-16 08:10:42 -0700

    Create pro52.py

---
+++ pro52.py
@@ -0,0 +1,12 @@
+a,b=list(map(int,input().split()))
+c,d=list(map(int,input().split()))
+e,f=list(map(int,input().split()))
+g,h=list(map(int,input().split()))
+m=d-b
+n=f-h
+o=e-c
+p=g-a
+if (m==n==o==p):
+    print("yes")
+else:
+    print("no")
