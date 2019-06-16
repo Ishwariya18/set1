abb8d51c66d1d33aab0cffbb4edbc1babf3681fe
Ishwariya18 <47315298+Ishwariya18@users.noreply.github.com>
2019-06-16 08:35:52 -0700

    Create pro55.py

---
+++ pro55.py
@@ -0,0 +1,21 @@
+a,b=map(int,input().split())
+z=list(map(int,input().split()))
+c,d=0,[]
+for i in range(0,len(z)):
+  t=i
+  for p in range(0,len(z)):
+    for l in range(0,b):
+      if l == b-1:
+        try:
+          c +=z[p+i]
+        except IndexError:
+            t = t-1
+            c +=z[t]
+      else:
+          c +=z[i]
+    d.append(c)
+    c=0
+ for i in range(0,len(z),b):
+    c = sum(z[i:i+b])
+    d.append(c)
+ print(*sorted(set(c)))
