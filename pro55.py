aec3532367b8e3ff320ceaf00ed30d5e6889dc0d
Ishwariya18 <47315298+Ishwariya18@users.noreply.github.com>
2019-06-16 08:38:25 -0700

    Update pro55.py

--- pro55.py
+++ pro55.py
@@ -1,21 +1,21 @@
-a,b=map(int,input().split())
-z=list(map(int,input().split()))
-c,d=0,[]
+a,b = map(int,input().split())
+z = list(map(int,input().split()))
+c,d = 0,[]
 for i in range(0,len(z)):
-  t=i
+  t = i
   for p in range(0,len(z)):
     for l in range(0,b):
       if l == b-1:
         try:
-          c +=z[p+i]
+          c += z[p+i]
         except IndexError:
             t = t-1
-            c +=z[t]
+            c += z[t]
       else:
-          c +=z[i]
+          c += z[i]
     d.append(c)
-    c=0
+    c = 0
  for i in range(0,len(z),b):
-    c = sum(z[i:i+b])
-    d.append(c)
+   c = sum(z[i:i+b])
+   d.append(c)
  print(*sorted(set(c)))
